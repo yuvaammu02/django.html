d830a866f7b183bef777cf93b30210611a59a133
yuvaammu02 <yuvaammu02@gmail.com>
2025-07-23 18:28:34 +0530

    django.html

--- frontend/settings.py
+++ frontend/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'frontend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project_django",
+        "USER": "postgres",
+        "PASSWORD": "ammu@2902",
+        "HOST": "127.0.0.1",
+        "PORT":"5432",
+        } 
 }
 
 
@@ -115,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
